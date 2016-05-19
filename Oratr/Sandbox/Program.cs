e0c98b52504adbd0099baf26f35236914654b73b
Jonathan Hinton <hintonjonathan@gmail.com>
2016-05-18 20:59:11 -0500

    explore msdn speech recognition capabilities

---
+++ Oratr/Sandbox/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Oratr.Sandbox
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
--- Oratr/Sandbox/Speech.cs
+++ Oratr/Sandbox/Speech.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Speech.Recognition;
+using System.Globalization;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace Oratr.Sandbox
 {
-    public class Speech
+    public partial class Form1 : Form
     {
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new CultureInfo("en-US"));
+            sre.SetInputToDefaultAudioDevice();
+
+            Choices colors = new Choices();
+            colors.Add(new string[] { "red", "green", "blue" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+
+            gb.Append(colors);
+
+            Grammar g = new Grammar(gb);
+
+            sre.LoadGrammar(g);
+
+            sre.SpeechRecognized +=
+                new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+            sre.Recognize();
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            MessageBox.Show("Speech Recognized: " + e.Result.Text);
+        }
     }
+
 }
