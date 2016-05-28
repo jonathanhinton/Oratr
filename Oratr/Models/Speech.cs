0ce6ff925e000d707ecec121535c9130ddb47af2
Jonathan Hinton <hintonjonathan@gmail.com>
2016-05-28 13:35:31 -0500

    create speech model, add columns for speech model table

---
+++ Oratr/Models/Speech.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oratr.Models
+{
+    public class Speech
+    {
+        public int SpeechId { get; set; }
+        public string SpeechTitle { get; set; }
+        public string SpeechBody { get; set; }
+        public TimeSpan TargetDeliveryTime { get; set; }
+
+        public virtual ApplicationUser CreatedBy { get; set; }
+    }
+}
