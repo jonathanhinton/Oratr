5d5fb210f5a548cdcb6e7cd1b1f89fdb72eb34a1
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-01 17:59:07 -0500

    add properties for results table

--- Oratr/Models/Results.cs
+++ Oratr/Models/Results.cs
@@ -7,5 +7,9 @@ namespace Oratr.Models
 {
     public class Results
     {
+        public int ResultsId { get; set; }
+        public int ActualWPM { get; set; }
+        public object[] DynamicsChart { get; set; }
+        public int UhUm { get; set; }
     }
 }
