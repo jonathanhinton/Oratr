227af6b26ab6ec1dc402540240c107042e470749
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-01 18:08:22 -0500

    unit testing boilerplate

---
+++ Oratr.Tests/DAL/OratrRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oratr.Models;
+using Oratr.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using Moq;
+
+
+namespace Oratr.Tests.DAL
+{
+    [TestClass]
+    public class OratrRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
