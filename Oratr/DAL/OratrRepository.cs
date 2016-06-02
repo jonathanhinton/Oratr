e2b8faedd6f2783ce4f7c4b31485978e3160f9c1
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-01 20:47:08 -0500

    create 2 constructors, one that will accept an argument for use in unit testing

--- Oratr/DAL/OratrRepository.cs
+++ Oratr/DAL/OratrRepository.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Oratr.Models;
+using System.Data.Entity;
 
 namespace Oratr.DAL
 {
     public class OratrRepository
     {
+        public OratrContext context { get; set; }
+        public IDbSet<ApplicationUser> Users { get { return context.Users; } }
+
+        public OratrRepository()
+        {
+            context = new OratrContext();
+        }
+
+        public OratrRepository(OratrContext _context)
+        {
+            context = _context;
+        }
     }
 }
