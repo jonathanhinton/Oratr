04af543db03ca6845f1338d40e78b6ab886fb1c9
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-01 20:46:21 -0500

    create public variables and mock tables for testing

--- Oratr.Tests/DAL/OratrRepositoryTest.cs
+++ Oratr.Tests/DAL/OratrRepositoryTest.cs
@@ -13,6 +13,36 @@ namespace Oratr.Tests.DAL
     [TestClass]
     public class OratrRepositoryTest
     {
+        Mock<OratrContext> mock_context { get; set; }
+        OratrRepository Repo { get; set;}
+
+        //Mock Speech Table
+        Mock<DbSet<Speech>> mock_speech_table { get; set; }
+        IQueryable<Speech> speech_data { get; set; }
+        List<Speech> speech_datasource { get; set; }
+
+        //Mock Result Table
+        Mock<DbSet<Result>> mock_result_table { get; set; }
+        IQueryable<Result> result_data { get; set; }
+        List<Result> result_datasource { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<OratrContext>();
+
+            speech_datasource = new List<Speech>();
+            result_datasource = new List<Result>();
+
+            mock_speech_table = new Mock<DbSet<Speech>>();
+            mock_result_table = new Mock<DbSet<Result>>();
+
+            Repo = new OratrRepository(mock_context.Object);
+
+            speech_data = speech_datasource.AsQueryable();
+            result_data = result_datasource.AsQueryable();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
