882c81f9738673ba4e5ab9fb9f306ab203cb578f
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-01 20:50:52 -0500

    connect mocks to datastore method

--- Oratr.Tests/DAL/OratrRepositoryTest.cs
+++ Oratr.Tests/DAL/OratrRepositoryTest.cs
@@ -43,6 +43,27 @@ namespace Oratr.Tests.DAL
             result_data = result_datasource.AsQueryable();
         }
 
+        void ConnectMocksToDatastore()
+        {
+            // Telling fake DbSet to use our datasource like something Queryable
+            mock_speech_table.As<IQueryable<Speech>>().Setup(m => m.GetEnumerator()).Returns(speech_data.GetEnumerator());
+            mock_speech_table.As<IQueryable<Speech>>().Setup(m => m.ElementType).Returns(speech_data.ElementType);
+            mock_speech_table.As<IQueryable<Speech>>().Setup(m => m.Expression).Returns(speech_data.Expression);
+            mock_speech_table.As<IQueryable<Speech>>().Setup(m => m.Provider).Returns(speech_data.Provider);
+
+            // Tell mocked VotrContext to use our fully mocked Datasource. (List<Speech>)
+            mock_context.Setup(m => m.Speeches).Returns(mock_speech_table.Object);
+
+            // Telling fake DbSet to use our datasource like something Queryable
+            mock_result_table.As<IQueryable<Result>>().Setup(m => m.GetEnumerator()).Returns(result_data.GetEnumerator());
+            mock_result_table.As<IQueryable<Result>>().Setup(m => m.ElementType).Returns(result_data.ElementType);
+            mock_result_table.As<IQueryable<Result>>().Setup(m => m.Expression).Returns(result_data.Expression);
+            mock_result_table.As<IQueryable<Result>>().Setup(m => m.Provider).Returns(result_data.Provider);
+
+            // Tell mocked VotrContext to use our fully mocked Datasource. (List<Result>)
+            mock_context.Setup(m => m.Results).Returns(mock_result_table.Object);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
