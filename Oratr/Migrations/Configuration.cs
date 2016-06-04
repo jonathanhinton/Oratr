47411f6a9573a9f41ba2c4ed292378eb6efb1882
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-04 10:17:36 -0500

    add configurations

--- Oratr/Migrations/Configuration.cs
+++ Oratr/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Oratr.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Oratr.DAL.OratrContext context)
