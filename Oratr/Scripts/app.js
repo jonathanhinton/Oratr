20cdb77c2a60666b5eab3006d8e0f1ec09efa5b1
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-13 18:51:47 -0500

    construct webkit speech object, define parameters

--- Oratr/Scripts/app.js
+++ Oratr/Scripts/app.js
@@ -1,1 +1,8 @@
-﻿
+﻿//construct recognition object
+var recognition = new webkitSpeechRecognition();
+//define parameters
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = "en-US";
+recognition.maxAlternatives = 1;
+
