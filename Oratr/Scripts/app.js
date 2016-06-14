9176c3e7b8c18bf3f4d1de97458815c00652c351
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-13 19:42:50 -0500

    boilerplate speech recognition

--- Oratr/Scripts/app.js
+++ Oratr/Scripts/app.js
@@ -1,8 +1,32 @@
 ﻿//construct recognition object
-var recognition = new webkitSpeechRecognition();
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+// var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; <-- I will implement this later
+var recognition = new SpeechRecognition();
 //define parameters
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.lang = "en-US";
 recognition.maxAlternatives = 1;
 
+recognition.onstart = function () {
+    //visible feedback goes here
+};
+
+recognition.onend = function () {
+    //again visible feedback goes here
+};
+
+recognition.onresult = function (event) {
+    if (typeof (event.results) === 'undefined') {
+        recognition.stop();
+        return;
+    }
+
+    for (var i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+            console.log("final results: " + event.results[i][0].transcript);
+        } else {
+            console.log("interim result: " + event.results[i][0].transcript);
+        }
+    } //end for loop
+}; //end onresult function
