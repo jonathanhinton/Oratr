5b44536c19be2ef2541cb37def1b44bbe5b7c331
Jonathan Hinton <hintonjonathan@gmail.com>
2016-06-19 10:13:19 -0700

    begin writing speech controller

--- Oratr/Scripts/app.js
+++ Oratr/Scripts/app.js
@@ -1,1 +1,16 @@
 ﻿var app = angular.module("OratrApp", []);
+
+app.controller("speechCtrl",
+    ['countdown',
+     'speechRecognition',
+     function (countdown, speechRecog) {
+         var self = this;
+
+         self.startSpeech = function (e) {
+             speechRecog.start();
+         }
+
+         self.stopSpeech = function (e) {
+             speechRecog.stop();
+         }
+}]);
